104c189a2463fbe81f0b03c24085f70e6a2f2b25
Marcos Feitoza <marcos.a.feitoza@gmail.com>
2025-02-23 19:18:21 -0500

    separando o style

--- src/screens/ConversionScreen.js
+++ src/screens/ConversionScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
+import styles from './styles';
 
 const units = [
   "ml", "L", "oz", "gal", "cm", "m", "inch", "ft", "mg", "g", "kg", "lb", "oz"
@@ -162,92 +163,3 @@ export default function ConversionScreen() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: '#fff',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-    textAlign: 'center',
-  },
-  label: {
-    fontSize: 18,
-    marginTop: 10,
-    marginBottom: 5,
-  },
-  inputGroup: {
-    marginBottom: 20,
-  },
-  picker: {
-    height: 50,
-    width: '100%',
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 8,
-    marginBottom: 10,
-  },
-  input: {
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 8,
-    paddingHorizontal: 10,
-    marginBottom: 10,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 20,
-  },
-  calculateButton: {
-    backgroundColor: '#007BFF',
-    padding: 15,
-    borderRadius: 8,
-    flex: 1,
-    marginRight: 10,
-    alignItems: 'center',
-  },
-  clearButton: {
-    backgroundColor: '#dc3545',
-    padding: 15,
-    borderRadius: 8,
-    flex: 1,
-    marginLeft: 10,
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  resultContainer: {
-    marginTop: 20,
-    padding: 15,
-    backgroundColor: '#f9f9f9',
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: '#ddd',
-    alignItems: 'center',
-  },
-  resultMessage: {
-    fontSize: 18,
-    color: '#333',
-    textAlign: 'center',
-  },
-  resultWinner: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#28a745', // Verde para o vencedor
-    marginTop: 10,
-  },
-  resultDifference: {
-    fontSize: 16,
-    color: '#666',
-    marginTop: 5,
-  },
-});
