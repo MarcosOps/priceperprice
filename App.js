0bae41e28460a0a4445d817dc1102504172858f2
Marcos Feitoza <marcos.a.feitoza@gmail.com>
2025-02-27 14:19:40 -0500

    clean up

--- App.js
+++ App.js
@@ -10,18 +10,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-// import React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { ThemeProvider } from './src/screens/ThemeContext';
-// import StackNavigator from './navigation/StackNavigator';
-
-// export default function App() {
-//   return (
-//     <ThemeProvider>
-//       <NavigationContainer>
-//         <StackNavigator />
-//       </NavigationContainer>
-//     </ThemeProvider>
-//   );
-// }
