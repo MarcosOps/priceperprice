4ffdbfb7645fd5712ac2cffe2c2eb01c0467d081
Marcos Feitoza <marcos.a.feitoza@gmail.com>
2025-04-26 17:11:54 -0400

    fix: fix style location

--- src/components/ProductInputGroup.js
+++ src/components/ProductInputGroup.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, TextInput } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
-import styles from '../screens/styles';
+import styles from '../styles/styles';
 import { units } from '../helpers/units';
 
 export default function ProductInputGroup({ unit, setUnit, quantity, setQuantity, price, setPrice, formatCurrency }) {
--- src/screens/ConversionScreen.js
+++ src/screens/ConversionScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
-import styles from './styles';
+import styles from '../styles/styles';
 
 import ColoredTitle from '../components/ColoredTitle';
 import ProductInputGroup from '../components/ProductInputGroup';
